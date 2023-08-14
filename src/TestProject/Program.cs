1c595956a348580e2224b82ddd69a802d983b919
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-08-14 23:49:29 +0300

    Добавил тестовый проект

---
+++ src/TestProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using MathExpressionParser;
+
+var _tokenizer = new Tokenizer();
+var _analyzer = new AbstractSyntaxTreeAnalyzer();
+var _calculator = new TreeCalculator();
+
+var bigInput = "(((2 + 2) * 2) / 4 + 0.5) * 2 ^ 2 - 10";
+var smallInput = "(2 + 2) * 2";
+
+try
+{
+    var input = smallInput;
+
+    Console.WriteLine($"Input: {input}\n");
+
+    Console.WriteLine("Tokens:");
+    var tokens = _tokenizer.Tokenize(input).Where(x => x.Type != TokenType.Space);
+    foreach (var token in tokens)
+    {
+        Console.WriteLine(token);
+    }
+    Console.WriteLine();
+    Console.WriteLine("AbstractSyntaxTreeAnalyzer is not implemented.\n");
+    Console.WriteLine("TreeCalculator is not implemented.\n");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+Console.ReadKey();
