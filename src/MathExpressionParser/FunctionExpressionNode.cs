c713f7f774dba761dadada0f371dc537cb300d76
Вадим Артюхов <artyuhovvadim@yandex.ru>
2023-08-16 23:48:11 +0300

    Добавил больше функций

--- src/MathExpressionParser/FunctionExpressionNode.cs
+++ src/MathExpressionParser/FunctionExpressionNode.cs
@@ -9,8 +9,21 @@ public class FunctionExpressionNode : ExpressionTreeNode
     private static readonly Dictionary<string, FunctionDefinition> FunctionsMap = new()
     {
         { "sin", new FunctionDefinition("sin", 1, args => Math.Sin(args[0])) },
+        { "asin", new FunctionDefinition("asin", 1, args => Math.Asin(args[0])) },
         { "cos", new FunctionDefinition("cos", 1, args => Math.Cos(args[0])) },
+        { "acos", new FunctionDefinition("acos", 1, args => Math.Acos(args[0])) },
+        { "tan", new FunctionDefinition("tan", 1, args => Math.Tan(args[0])) },
+        { "atan", new FunctionDefinition("atan", 1, args => Math.Atan(args[0])) },
         { "pow", new FunctionDefinition("pow", 2, args => Math.Pow(args[0], args[1])) },
+        { "abs", new FunctionDefinition("abs", 1, args => Math.Abs(args[0])) },
+        { "sqrt", new FunctionDefinition("sqrt", 1, args => Math.Sqrt(args[0])) },
+        { "log", new FunctionDefinition("log", 2, args => Math.Log(args[0], args[1])) },
+        { "lg", new FunctionDefinition("lg", 1, args => Math.Log10(args[0])) },
+        { "ln", new FunctionDefinition("ln", 1, args => Math.Log(args[0])) },
+        { "exp", new FunctionDefinition("exp", 1, args => Math.Exp(args[0])) },
+        { "max", new FunctionDefinition("max", 2, args => Math.Max(args[0], args[1])) },
+        { "min", new FunctionDefinition("min", 2, args => Math.Min(args[0], args[1])) },
+        { "sign", new FunctionDefinition("sign", 1, args => Math.Sign(args[0])) },
     };
 
     private readonly List<ExpressionTreeNode> _arguments;
